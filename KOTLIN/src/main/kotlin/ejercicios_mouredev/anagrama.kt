17cd6cf58261859d36eb97c0f15efeaf03fe74e0
danie <daniel2841995@gmail.com>
2023-09-18 11:45:20 +0200

    Actualizacion de ejercicios

---
+++ KOTLIN/src/main/kotlin/ejercicios_mouredev/anagrama.kt
@@ -0,0 +1,50 @@
+/*
+ * Reto #1
+ * ¿ES UN ANAGRAMA?
+ * Fecha publicación enunciado: 03/01/22
+ * Fecha publicación resolución: 10/01/22
+ * Dificultad: MEDIA
+ *
+ * Enunciado: Escribe una función que reciba dos palabras (String) y retorne verdadero o falso (Boolean) según sean o no anagramas.
+ * Un Anagrama consiste en formar una palabra reordenando TODAS las letras de otra palabra inicial.
+ * NO hace falta comprobar que ambas palabras existan.
+ * Dos palabras exactamente iguales no son anagrama.
+ *
+ * Información adicional:
+ * - Usa el canal de nuestro discord (https://mouredev.com/discord) "🔁reto-semanal" para preguntas, dudas o prestar ayuda a la acomunidad.
+ * - Puedes hacer un Fork del repo y una Pull Request al repo original para que veamos tu solución aportada.
+ * - Revisaré el ejercicio en directo desde Twitch el lunes siguiente al de su publicación.
+ * - Subiré una posible solución al ejercicio el lunes siguiente al de su publicación.
+ *
+ */
+fun anagrama(): Boolean {
+    print("Primera palabra: ")
+    val palabra_one: String = readln().toString()
+    print("Segunda palabra: ")
+    val palabra_two: String = readln().toString()
+    if (palabra_one == palabra_two){
+        print("No son anagramas por ser iguales")
+        return false
+    }
+    val palabra_one_list = mutableListOf<Char>()
+    val palabra_two_list = mutableListOf<Char>()
+    for (index in palabra_one) {
+        palabra_one_list.add(index)
+        println(index)
+    }
+    for (index in palabra_two) {
+        palabra_two_list.add(index)
+        println(index)
+    }
+    palabra_one_list.sort()
+    palabra_two_list.sort()
+    if (palabra_one_list.equals(palabra_two_list)) {
+        println("Son anagramas")
+        return true
+
+    }
+    else {
+        print("No son anagramas")
+        return false
+    }
+}
---
+++ KOTLIN/src/main/kotlin/ejercicios_mouredev/pruebas.kt
@@ -0,0 +1,33 @@
+fun pruebas() {
+    val listaColores = mutableListOf("Azul", "Rojo", "Verde")
+    println(listaColores)
+    listaColores.remove("Azul")
+    listaColores.removeAt(1)
+    listaColores.add(0, "Rosa")
+    println(listaColores)
+    println(listaColores.get(0))
+    println(listaColores.indexOf("Rosa"))
+    listaColores.add("Fucsia")
+    listaColores.add("Blanco")
+    listaColores.add("Negro")
+    println(listaColores)
+
+    val objetosguapos = mutableListOf<String>()
+    objetosguapos.add("Sofá")
+    print(objetosguapos)
+
+    val str = "Había una vez unn circo que llenaba el mundo de ilusión"
+    val delim = " "
+    val listacirco = str.split(delim)
+    print(listacirco)
+
+    var listacreadaconrango = 0.rangeTo(6) // Crea lista desde el 0 al 6
+    println (listacreadaconrango)
+    var listacreadaconrango_2 = 1..6 // Crea lista desde el 0 al 6
+    println (listacreadaconrango_2)
+    var listacreadaconrango_3 = 1 until 6 // Crea lista desde el 0 al 5
+    println (listacreadaconrango_3)
+    println(3 in listacreadaconrango_3) // Devuelve true
+
+
+}
